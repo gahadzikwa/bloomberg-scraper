95da03ed9831a42760b92ed26831a7a91a4808d5
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 21:01:11 +0700

    Create FundsListScraper class to scrape funds list pages

---
+++ classes/FundsListScraper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * FundsListScraper - Class for scraping funds list page
+ * 
+ * @author  Risan Bagja Pradana <risanbagja@yahoo.com>
+ * @package BloombergScraper
+ * @version 1.0
+ */
+namespace BloombergScraper;
+
+class FundsListScraper
+{
+    
+}
