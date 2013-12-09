38ebe65c6b224cbeff2790f2a08d3ad1ee1a50d4
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 18:56:49 +0700

    Add configuration file

---
+++ config/global.php
@@ -0,0 +1,6 @@
+<?php
+/* Bloomberg site URL */
+define('BLOOMBERG_URL', 'http://www.bloomberg.com/');
+
+/* URL of funds list in Indonesia */
+define('FUNDS_LIST_URL', 'http://www.bloomberg.com/markets/funds/country/indonesia/');
