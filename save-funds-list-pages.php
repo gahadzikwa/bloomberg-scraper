5654fa435c51b78b52c6c853a9570695e1fa2460
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 20:48:23 +0700

    Add implementation to save funds list pages

--- save-funds-list-pages.php
+++ save-funds-list-pages.php
@@ -1,6 +1,17 @@
 <?php
-/* Include needed classes */
+/* Include global configuration file */
+require_once('config/global.php');
+
+/* Include SavePages class */
 require_once('classes/SavePages.php');
 
+/* Create SavePages instance */
 $savePages = new \BloombergScraper\SavePages();
-$savePages->saveFundsList();
+
+/* Save funds list pages */
+$pagesSaved = $savePages->saveFundsList(3);
+
+/* Echoing saved result */
+echo 'Bloomberg Indonesia funds list pages successfuly saved.<br>';
+echo 'Pages saved: ' . $pagesSaved . '<br>';
+echo '<a href="' . FUNDS_LIST_PAGES_DIR . '">Go to saved fund list pages directory</a>';
