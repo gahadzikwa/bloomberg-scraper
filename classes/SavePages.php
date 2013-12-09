fcb87eac16bd7884c996eff8ac48ee5cc3d3a4d8
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 18:53:44 +0700

    Add an implementation for save funds list pages

--- classes/SavePages.php
+++ classes/SavePages.php
@@ -1,10 +1,23 @@
 <?php
-namespace BloombergScrapper;
+namespace BloombergScraper;
 
+
+/**
+ * SavePages class - A class for saving Bloomberg pages
+ * 
+ * @author  Risan Bagja Pradana <risanbagja@yahoo.com>
+ * @package BloombergScarper
+ * @version 1.0
+ */
 class SavePages
 {
+    /**
+     * Method to save Indonesia funds list pages
+     * 
+     * @access  public
+     */
     public function saveFundsList()
     {
-        
+
     }
 }
---
+++ save-funds-list-pages.php
@@ -0,0 +1,6 @@
+<?php
+/* Include needed classes */
+require_once('classes/SavePages.php');
+
+$savePages = new SavePages();
+$savePages->saveFundsList();
