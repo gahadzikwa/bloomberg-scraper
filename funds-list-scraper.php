9d664f07f780944520385c3df5edc18c9fad4f47
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 23:37:31 +0700

    Modify new implementation

--- funds-list-scraper.php
+++ funds-list-scraper.php
@@ -11,4 +11,9 @@ require_once('classes/FundsListScraper.php');
 $fundsListScraper = new \BloombergScraper\FundsListScraper();
 
 /* Scrape funds list */
-$fundsListScraper->run();
+$totalFunds = $fundsListScraper->run();
+
+/* Echoing saved result */
+echo 'Bloomberg Indonesia funds list pages successfuly scraped.<br>';
+echo 'Total funds scraped: ' . $totalFunds . '<br>';
+echo '<a href="' . FUNDS_LIST_PAGES_DIR . '">Go to saved fund list pages directory</a>';
--- save-funds-list-pages.php
+++ save-funds-list-pages.php
@@ -16,4 +16,4 @@ $pagesSaved = $savePages->saveFundsList(3);
 /* Echoing saved result */
 echo 'Bloomberg Indonesia funds list pages successfuly saved.<br>';
 echo 'Pages saved: ' . $pagesSaved . '<br>';
-echo '<a href="' . FUNDS_LIST_PAGES_DIR . '">Go to saved fund list pages directory</a>';
+echo '<a href="' . FUNDS_LIST_FILE . '">Download scraped funds list</a>';
