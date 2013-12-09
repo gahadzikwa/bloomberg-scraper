ba6ce91cf523871b6f8e06820e02012c2133ad33
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 21:33:05 +0700

    Add a class constructor

--- classes/FundsListScraper.php
+++ classes/FundsListScraper.php
@@ -12,14 +12,50 @@ use DomXPath;
 
 class FundsListScraper
 {
+    /**
+     * An instance of DomDocument class
+     * 
+     * @var     object  $dom
+     * @access  private
+     */
+    private $dom;
+
+
+    /**
+     * An instance of DomXPath class
+     * 
+     * @var     object  $xpath
+     * @access  private
+     */
+    private $xpath;
+
+
+    /**
+     * A class constructor
+     * 
+     * @return  object  An instance of FundsListScraper class
+     * @access  public
+     */
+    public function __construct()
+    {
+        /* Create new DomDocument object */
+        $this->dom = new DomDocument();
+    }
+
+
+    /**
+     * Main function to scrape funds list data
+     * 
+     * @access  public
+     */
     public function run()
     {
-        $dom = new DomDocument();
-        @$dom->load(FUNDS_LIST_PAGES_DIR . '1' . SAVED_PAGE_EXT);
-        $xPath = new DomXPath($dom);
+        @$this->dom->load(FUNDS_LIST_PAGES_DIR . '1' . SAVED_PAGE_EXT);
+        $this->xpath = new DomXPath($this->dom);
+
         $className = 'ticker_data';
         $nodes = $xPath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $className ')]");
         echo '<pre>';
         print_r($nodes->item(0));
-    } 
+    }
 }
