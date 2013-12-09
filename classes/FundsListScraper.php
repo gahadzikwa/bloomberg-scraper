c6721016d5960d6e2277175615f4185d1c2c4b5b
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 21:24:47 +0700

    Use DomDocument and DomXPath class, create a simple implementation to scrape funds list table

--- classes/FundsListScraper.php
+++ classes/FundsListScraper.php
@@ -7,12 +7,19 @@
  * @version 1.0
  */
 namespace BloombergScraper;
+use DomDocument;
+use DomXPath;
 
 class FundsListScraper
 {
     public function run()
     {
-        $html = file_get_contents(FUNDS_LIST_PAGES_DIR . '1' . SAVED_PAGE_EXT);
-        echo $html;
+        $dom = new DomDocument();
+        @$dom->load(FUNDS_LIST_PAGES_DIR . '1' . SAVED_PAGE_EXT);
+        $xPath = new DomXPath($dom);
+        $className = 'ticker_data';
+        $nodes = $xPath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $className ')]");
+        echo '<pre>';
+        print_r($nodes->item(0));
     } 
 }
