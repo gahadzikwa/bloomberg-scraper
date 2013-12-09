9537ceae081a926e0f1f1b02495e22cd6aedb3b7
Risan Bagja <risanbagja@yahoo.com>
2013-12-09 20:40:04 +0700

    Add configuration constants for saving pages

--- config/global.php
+++ config/global.php
@@ -4,3 +4,10 @@ define('BLOOMBERG_URL', 'http://www.bloomberg.com/');
 
 /* URL of funds list in Indonesia */
 define('FUNDS_LIST_URL', 'http://www.bloomberg.com/markets/funds/country/indonesia/');
+
+/* Saved pages extension */
+define('SAVED_PAGE_EXT', '.html');
+/* Bloomberg saved pages directory */
+define('SAVED_PAGES_DIR', 'bloomberg-pages');
+/* Bloomberg funds list pages directory */
+define('FUNDS_LIST_PAGES_DIR', SAVED_PAGES_DIR . 'funds-list');
