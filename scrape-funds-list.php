ffaa0dc7cf630965cbefbe09a4b4d4b4695abae8
Risan Bagja <risanbagja@yahoo.com>
2014-01-15 20:57:37 +0700

    Add an implementation of funds list scraper

---
+++ scrape-funds-list.php
@@ -0,0 +1,15 @@
+<?php
+require_once('classes/FundsListScraper.php');
+
+$scraper = new FundsListScraper();
+$scraper->scrape()->save('data/funds-list.json'); ?>
+
+<p>
+    <strong>Total Funds Scraped:</strong>
+    <?php echo $scraper->getTotalFunds(); ?>
+</p>
+<p>
+    <a href="<?php echo $scraper->getFilename(); ?>">
+        Result Data
+    </a>
+</p>
